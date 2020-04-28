f591f956384ccf41892fc7f0e18d8d141fbde4ba
Grace Pelella <gmpelella@wpi.edu>
2020-04-28 12:20:30 -0400

    hunger data good to go

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 let parsedData;
 
+// box and whisker shit:
+//HUNGER
+let bwA0H = [];
+let bwA0H0 = [];
+let bwA0H1 = [];
+let bwA0H2 = [];
+let bwA0H3 = [];
+let bwA0H4 = [];
+let bwA1H = [];
+let bwA1H0 = [];
+let bwA1H1 = [];
+let bwA1H2 = [];
+let bwA1H3 = [];
+let bwA1H4 = [];
+let bwA2H = [];
+let bwA2H0 = [];
+let bwA2H1 = [];
+let bwA2H2 = [];
+let bwA2H3 = [];
+let bwA2H4 = [];
+let bwA3H = [];
+let bwA3H0 = [];
+let bwA3H1 = [];
+let bwA3H2 = [];
+let bwA3H3 = [];
+let bwA3H4 = [];
+let bwA4H = [];
+let bwA4H0 = [];
+let bwA4H1 = [];
+let bwA4H2 = [];
+let bwA4H3 = [];
+let bwA4H4 = [];
+
+
 //parsed data - organize data
 function parseData(data){
     //for each row of the data, organize the columns
@@ -7,6 +41,216 @@ function parseData(data){
         let id = d.uuid;
 
         //need to make sure all the values except the uuid is counted as an int - not string
+        //age 0
+        d["age0Data__hunger0"] =     +d["age0Data__hunger0"];
+        d["age0Data__military0"] =   +d["age0Data__military0"];
+        d["age0Data__population0"] = +d["age0Data__population0"];
+        d["age0Data__science0"] =    +d["age0Data__science0"];
+        d["age0Data__security0"] =   +d["age0Data__security0"];
+        d["age0Data__time0"] =       +d["age0Data__time0"];
+        d["age0Data__hunger1"] =     +d["age0Data__hunger1"];
+        d["age0Data__military1"] =   +d["age0Data__military1"];
+        d["age0Data__population1"] = +d["age0Data__population1"];
+        d["age0Data__science1"] =    +d["age0Data__science1"];
+        d["age0Data__security1"] =   +d["age0Data__security1"];
+        d["age0Data__time1"] =       +d["age0Data__time1"];
+        d["age0Data__hunger2"] =     +d["age0Data__hunger2"];
+        d["age0Data__military2"] =   +d["age0Data__military2"];
+        d["age0Data__population2"] = +d["age0Data__population2"];
+        d["age0Data__science2"] =    +d["age0Data__science2"];
+        d["age0Data__security2"] =   +d["age0Data__security2"];
+        d["age0Data__time2"] =       +d["age0Data__time2"];
+        d["age0Data__hunger3"] =     +d["age0Data__hunger3"];
+        d["age0Data__military3"] =   +d["age0Data__military3"];
+        d["age0Data__population3"] = +d["age0Data__population3"];
+        d["age0Data__science3"] =    +d["age0Data__science3"];
+        d["age0Data__security3"] =   +d["age0Data__security3"];
+        d["age0Data__time3"] =       +d["age0Data__time3"];
+        d["age0Data__hunger4"] =     +d["age0Data__hunger4"];
+        d["age0Data__military4"] =   +d["age0Data__military4"];
+        d["age0Data__population4"] = +d["age0Data__population4"];
+        d["age0Data__science4"] =    +d["age0Data__science4"];
+        d["age0Data__security4"] =   +d["age0Data__security4"];
+        d["age0Data__time4"] =       +d["age0Data__time4"];
+        //age 1
+        d["age1Data__hunger0"] =     +d["age1Data__hunger0"];
+        d["age1Data__military0"] =   +d["age1Data__military0"];
+        d["age1Data__population0"] = +d["age1Data__population0"];
+        d["age1Data__science0"] =    +d["age1Data__science0"];
+        d["age1Data__security0"] =   +d["age1Data__security0"];
+        d["age1Data__time0"] =       +d["age1Data__time0"];
+        d["age1Data__hunger1"] =     +d["age1Data__hunger1"];
+        d["age1Data__military1"] =   +d["age1Data__military1"];
+        d["age1Data__population1"] = +d["age1Data__population1"];
+        d["age1Data__science1"] =    +d["age1Data__science1"];
+        d["age1Data__security1"] =   +d["age1Data__security1"];
+        d["age1Data__time1"] =       +d["age1Data__time1"];
+        d["age1Data__hunger2"] =     +d["age1Data__hunger2"];
+        d["age1Data__military2"] =   +d["age1Data__military2"];
+        d["age1Data__population2"] = +d["age1Data__population2"];
+        d["age1Data__science2"] =    +d["age1Data__science2"];
+        d["age1Data__security2"] =   +d["age1Data__security2"];
+        d["age1Data__time2"] =       +d["age1Data__time2"];
+        d["age1Data__hunger3"] =     +d["age1Data__hunger3"];
+        d["age1Data__military3"] =   +d["age1Data__military3"];
+        d["age1Data__population3"] = +d["age1Data__population3"];
+        d["age1Data__science3"] =    +d["age1Data__science3"];
+        d["age1Data__security3"] =   +d["age1Data__security3"];
+        d["age1Data__time3"] =       +d["age1Data__time3"];
+        d["age1Data__hunger4"] =     +d["age1Data__hunger4"];
+        d["age1Data__military4"] =   +d["age1Data__military4"];
+        d["age1Data__population4"] = +d["age1Data__population4"];
+        d["age1Data__science4"] =    +d["age1Data__science4"];
+        d["age1Data__security4"] =   +d["age1Data__security4"];
+        d["age1Data__time4"] =       +d["age1Data__time4"];
+        //age 2
+        d["age2Data__hunger0"] =     +d["age2Data__hunger0"];
+        d["age2Data__military0"] =   +d["age2Data__military0"];
+        d["age2Data__population0"] = +d["age2Data__population0"];
+        d["age2Data__science0"] =    +d["age2Data__science0"];
+        d["age2Data__security0"] =   +d["age2Data__security0"];
+        d["age2Data__time0"] =       +d["age2Data__time0"];
+        d["age2Data__hunger1"] =     +d["age2Data__hunger1"];
+        d["age2Data__military1"] =   +d["age2Data__military1"];
+        d["age2Data__population1"] = +d["age2Data__population1"];
+        d["age2Data__science1"] =    +d["age2Data__science1"];
+        d["age2Data__security1"] =   +d["age2Data__security1"];
+        d["age2Data__time1"] =       +d["age2Data__time1"];
+        d["age2Data__hunger2"] =     +d["age2Data__hunger2"];
+        d["age2Data__military2"] =   +d["age2Data__military2"];
+        d["age2Data__population2"] = +d["age2Data__population2"];
+        d["age2Data__science2"] =    +d["age2Data__science2"];
+        d["age2Data__security2"] =   +d["age2Data__security2"];
+        d["age2Data__time2"] =       +d["age2Data__time2"];
+        d["age2Data__hunger3"] =     +d["age2Data__hunger3"];
+        d["age2Data__military3"] =   +d["age2Data__military3"];
+        d["age2Data__population3"] = +d["age2Data__population3"];
+        d["age2Data__science3"] =    +d["age2Data__science3"];
+        d["age2Data__security3"] =   +d["age2Data__security3"];
+        d["age2Data__time3"] =       +d["age2Data__time3"];
+        d["age2Data__hunger4"] =     +d["age2Data__hunger4"];
+        d["age2Data__military4"] =   +d["age2Data__military4"];
+        d["age2Data__population4"] = +d["age2Data__population4"];
+        d["age2Data__science4"] =    +d["age2Data__science4"];
+        d["age2Data__security4"] =   +d["age2Data__security4"];
+        d["age2Data__time4"] =       +d["age2Data__time4"];
+        //age 3
+        d["age3Data__hunger0"] =     +d["age3Data__hunger0"];
+        d["age3Data__military0"] =   +d["age3Data__military0"];
+        d["age3Data__population0"] = +d["age3Data__population0"];
+        d["age3Data__science0"] =    +d["age3Data__science0"];
+        d["age3Data__security0"] =   +d["age3Data__security0"];
+        d["age3Data__time0"] =       +d["age3Data__time0"];
+        d["age3Data__hunger1"] =     +d["age3Data__hunger1"];
+        d["age3Data__military1"] =   +d["age3Data__military1"];
+        d["age3Data__population1"] = +d["age3Data__population1"];
+        d["age3Data__science1"] =    +d["age3Data__science1"];
+        d["age3Data__security1"] =   +d["age3Data__security1"];
+        d["age3Data__time1"] =       +d["age3Data__time1"];
+        d["age3Data__hunger2"] =     +d["age3Data__hunger2"];
+        d["age3Data__military2"] =   +d["age3Data__military2"];
+        d["age3Data__population2"] = +d["age3Data__population2"];
+        d["age3Data__science2"] =    +d["age3Data__science2"];
+        d["age3Data__security2"] =   +d["age3Data__security2"];
+        d["age3Data__time2"] =       +d["age3Data__time2"];
+        d["age3Data__hunger3"] =     +d["age3Data__hunger3"];
+        d["age3Data__military3"] =   +d["age3Data__military3"];
+        d["age3Data__population3"] = +d["age3Data__population3"];
+        d["age3Data__science3"] =    +d["age3Data__science3"];
+        d["age3Data__security3"] =   +d["age3Data__security3"];
+        d["age3Data__time3"] =       +d["age3Data__time3"];
+        d["age3Data__hunger4"] =     +d["age3Data__hunger4"];
+        d["age3Data__military4"] =   +d["age3Data__military4"];
+        d["age3Data__population4"] = +d["age3Data__population4"];
+        d["age3Data__science4"] =    +d["age3Data__science4"];
+        d["age3Data__security4"] =   +d["age3Data__security4"];
+        d["age3Data__time4"] =       +d["age3Data__time4"];
+        //age 4
+        d["age4Data__hunger0"] =     +d["age4Data__hunger0"];
+        d["age4Data__military0"] =   +d["age4Data__military0"];
+        d["age4Data__population0"] = +d["age4Data__population0"];
+        d["age4Data__science0"] =    +d["age4Data__science0"];
+        d["age4Data__security0"] =   +d["age4Data__security0"];
+        d["age4Data__time0"] =       +d["age4Data__time0"];
+        d["age4Data__hunger1"] =     +d["age4Data__hunger1"];
+        d["age4Data__military1"] =   +d["age4Data__military1"];
+        d["age4Data__population1"] = +d["age4Data__population1"];
+        d["age4Data__science1"] =    +d["age4Data__science1"];
+        d["age4Data__security1"] =   +d["age4Data__security1"];
+        d["age4Data__time1"] =       +d["age4Data__time1"];
+        d["age4Data__hunger2"] =     +d["age4Data__hunger2"];
+        d["age4Data__military2"] =   +d["age4Data__military2"];
+        d["age4Data__population2"] = +d["age4Data__population2"];
+        d["age4Data__science2"] =    +d["age4Data__science2"];
+        d["age4Data__security2"] =   +d["age4Data__security2"];
+        d["age4Data__time2"] =       +d["age4Data__time2"];
+        d["age4Data__hunger3"] =     +d["age4Data__hunger3"];
+        d["age4Data__military3"] =   +d["age4Data__military3"];
+        d["age4Data__population3"] = +d["age4Data__population3"];
+        d["age4Data__science3"] =    +d["age4Data__science3"];
+        d["age4Data__security3"] =   +d["age4Data__security3"];
+        d["age4Data__time3"] =       +d["age4Data__time3"];
+        d["age4Data__hunger4"] =     +d["age4Data__hunger4"];
+        d["age4Data__military4"] =   +d["age4Data__military4"];
+        d["age4Data__population4"] = +d["age4Data__population4"];
+        d["age4Data__science4"] =    +d["age4Data__science4"];
+        d["age4Data__security4"] =   +d["age4Data__security4"];
+        d["age4Data__time4"] =       +d["age4Data__time4"];
+
+        //box&whisker plot data:
+        //HUNGER
+        bwA0H.push(d["age0Data__hunger0"],
+            d["age0Data__hunger1"],
+            d["age0Data__hunger2"],
+            d["age0Data__hunger3"],
+            d["age0Data__hunger4"]);
+        bwA0H0.push(d["age0Data__hunger0"]);
+        bwA0H1.push(d["age0Data__hunger1"]);
+        bwA0H2.push(d["age0Data__hunger2"]);
+        bwA0H3.push(d["age0Data__hunger3"]);
+        bwA0H4.push(d["age0Data__hunger4"]);
+        bwA1H.push(d["age1Data__hunger0"],
+            d["age1Data__hunger1"],
+            d["age1Data__hunger2"],
+            d["age1Data__hunger3"],
+            d["age1Data__hunger4"]);
+        bwA1H0.push(d["age1Data__hunger0"]);
+        bwA1H1.push(d["age1Data__hunger1"]);
+        bwA1H2.push(d["age1Data__hunger2"]);
+        bwA1H3.push(d["age1Data__hunger3"]);
+        bwA1H4.push(d["age1Data__hunger4"]);
+        bwA2H.push(d["age2Data__hunger0"],
+            d["age2Data__hunger1"],
+            d["age2Data__hunger2"],
+            d["age2Data__hunger3"],
+            d["age2Data__hunger4"]);
+        bwA2H0.push(d["age2Data__hunger0"]);
+        bwA2H1.push(d["age2Data__hunger1"]);
+        bwA2H2.push(d["age2Data__hunger2"]);
+        bwA2H3.push(d["age2Data__hunger3"]);
+        bwA2H4.push(d["age2Data__hunger4"]);
+        bwA3H.push(d["age3Data__hunger0"],
+            d["age3Data__hunger1"],
+            d["age3Data__hunger2"],
+            d["age3Data__hunger3"],
+            d["age3Data__hunger4"]);
+        bwA3H0.push(d["age3Data__hunger0"]);
+        bwA3H1.push(d["age3Data__hunger1"]);
+        bwA3H2.push(d["age3Data__hunger2"]);
+        bwA3H3.push(d["age3Data__hunger3"]);
+        bwA3H4.push(d["age3Data__hunger4"]);
+        bwA4H.push(d["age4Data__hunger0"],
+            d["age4Data__hunger1"],
+            d["age4Data__hunger2"],
+            d["age4Data__hunger3"],
+            d["age4Data__hunger4"]);
+        bwA4H0.push(d["age4Data__hunger0"]);
+        bwA4H1.push(d["age4Data__hunger1"]);
+        bwA4H2.push(d["age4Data__hunger2"]);
+        bwA4H3.push(d["age4Data__hunger3"]);
+        bwA4H4.push(d["age4Data__hunger4"]);
+
+
 
         //wait do i need this here? // i dont think so
         //age 0: stone age
